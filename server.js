caf3aa55c5e258f4152dc9fec02cde45c3373857
Flavien Cocu <Flavien.cocu@insa-rouen.fr>
2020-05-14 16:30:20 +0200

    delete cors

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-const cors = require('cors');
 
-ngApp.use(cors());
 ngApp.use(express.static('./dist/tfm-bpmn-to-trello'));
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/tfm-bpmn-to-trello/index.html'));
